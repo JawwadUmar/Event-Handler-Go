5429a92da3804c2df5c11037d61e6e248b0f4b93
Jawwad Umar <you@example.com>
2025-12-18 02:40:08 +0530

    added GET and POST

--- main.go
+++ main.go
@@ -3,16 +3,44 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/event", getEvent)
+	server.GET("/events", getEvent)
+	server.POST("/events", createEvent)
+
 	server.Run(":8081")
 }
 
 func getEvent(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"Fhd": "fzi"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse the request data",
+		})
+
+		return
+	}
+
+	event.Id = 1
+	event.UserId = 1
+
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "Event created :)",
+		"event":   event,
+	})
+
+	event.Save()
+
 }
