636f8b40c559e4756dcc53e026b8108081e8e347
Jawwad Umar <you@example.com>
2026-01-05 22:33:09 +0530

    added the protected routes in one group

--- routes/route.go
+++ routes/route.go
@@ -16,11 +16,14 @@ func registerRoutesForEvent(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 
-	// authGroup := server.Group("/")
-	// authGroup.
-	server.POST("/events", middlewares.Authentication, createEvent)
-	server.DELETE("/events/:id", delteEvent)
-	server.PUT("/events/:id", updateEvent)
+	authenticationRequiredGroup := server.Group("/")
+	authenticationRequiredGroup.Use(middlewares.Authentication)
+	authenticationRequiredGroup.POST("/events", createEvent)
+	authenticationRequiredGroup.DELETE("/events/:id", delteEvent)
+	authenticationRequiredGroup.PUT("/events/:id", updateEvent)
+	// server.POST("/events", middlewares.Authentication, createEvent)
+	// server.DELETE("/events/:id", delteEvent)
+	// server.PUT("/events/:id", updateEvent)
 	server.DELETE("/events", emptyTable)
 }
 
